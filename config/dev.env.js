60c99557108fae5419b928b6e816f4207e81e64c
YJ <yongjoo.kim@onionground.com>
2018-04-17 23:08:33 +0900

    Modify - HOST 오류 변경

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,5 +8,5 @@ module.exports = merge(prodEnv, {
   API_URL: '"http://dev-api.zuly.co.kr/api/v1.0"',
   API_MESSAGE_URL: '"http://localhost:4000/api/v1.0"',
   API_IMAGE_URL: '"http://dev-image.zuly.co.kr/"',
-  HOST: '"dev-front.zuly.co.kr"',
+  HOST: '"zuly.co.kr"',
 })
