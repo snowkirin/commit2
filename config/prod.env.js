f2a3e6c83685eef03d71069368337c436d93899e
Ellen <ellen0820@onionground.com>
2018-04-17 20:46:02 +0900

    modify HOST domain 수정

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@ module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"https://api.zuly.co.kr/api/v1.0"',
   API_MESSAGE_URL: '"http://message.zuly.co.kr/api/v1.0"',
-  API_IMAGE_URL: '"http://image.zuly.co.kr/"',  
-  HOST: '"www.zuly.co.kr"',
+  API_IMAGE_URL: '"http://image.zuly.co.kr/"',
+  HOST: '"zuly.co.kr"',
 }
