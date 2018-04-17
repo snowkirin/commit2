16f604e92b03f2ac361a8d5d295cfcdf0320818f
YJ <yongjoo.kim@onionground.com>
2018-04-17 17:53:14 +0900

    Modify - 설정파일 변경

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,8 +5,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:3000/api/v1.0"',
+  API_URL: '"http://dev-api.zuly.co.kr/api/v1.0"',
   API_MESSAGE_URL: '"http://localhost:4000/api/v1.0"',
   API_IMAGE_URL: '"http://dev-image.zuly.co.kr/"',
-  HOST: '"localhost"',
+  HOST: '"dev-front.zuly.co.kr"',
 })
