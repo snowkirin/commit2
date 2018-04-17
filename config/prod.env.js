53db2421e43e722aa936fccd387062a72945af2c
YJ <yongjoo.kim@onionground.com>
2018-04-17 17:57:33 +0900

    Modify - API 프로토콜 수정

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://api.zuly.co.kr/api/v1.0"',
+  API_URL: '"https://api.zuly.co.kr/api/v1.0"',
   API_MESSAGE_URL: '"http://message.zuly.co.kr/api/v1.0"',
   API_IMAGE_URL: '"http://image.zuly.co.kr/"',  
   HOST: '"www.zuly.co.kr"',
