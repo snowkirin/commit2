1220ef3dc61e872c46cf4fb964817cb2c9a768c4
YJ <yongjoo.kim@onionground.com>
2018-04-17 10:55:20 +0900

    Modify - Production 설정 변경

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://dev-api.zuly.co.kr/api/v1.0"',
+  API_URL: '"http://api.zuly.co.kr/api/v1.0"',
   API_MESSAGE_URL: '"http://message.zuly.co.kr/api/v1.0"',
-  API_IMAGE_URL: '"http://dev-image.zuly.co.kr/"',  
-  HOST: '"zuly.co.kr"',
+  API_IMAGE_URL: '"http://image.zuly.co.kr/"',  
+  HOST: '"www.zuly.co.kr"',
 }
