96d2d17f4eec670400712da0df3fcf9872bdd92a
YJ <yongjoo.kim@onionground.com>
2018-04-20 12:18:32 +0900

    Add - 로컬 테스트 세팅 추가.

---
+++ config/local.env.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const merge = require('webpack-merge')
+
+module.exports = {
+  NODE_ENV: '"local"',
+  API_URL: '"http://localhost:3000/api/v1.0"',
+  API_MESSAGE_URL: '"http://localhost:4000/api/v1.0"',
+  API_IMAGE_URL: '"http://dev-image.zuly.co.kr/"',
+  HOST: '"localhost"',
+  GA_ID: '"UA-113930121-1"',
+  TOKEN_NAME: '"local_access_token"',
+}
