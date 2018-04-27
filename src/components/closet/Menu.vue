7489abaf9f47ff8dd29e7f6e8fcefc200eb24284
Ellen <ellen0820@onionground.com>
2018-04-27 14:27:00 +0900

    modify click 이벤트 먹도록 수정

--- src/components/closet/Menu.vue
+++ src/components/closet/Menu.vue
@@ -2,35 +2,35 @@
   <div class="closet-mobile-menu">
     <div class="closet-menu mt30">
       <router-link to="/closet/tomorrow">
-        <div id="tomorrow" class="menu" data-id="tomorrow" :class="{ 'closet_active' : activeMenuName === 'tomorrow' }">
+        <div id="tomorrow" class="menu" data-id="tomorrow" :class="{ 'closet_active' : activeMenuName === 'tomorrow' }" @click="menuClick('tomorrow')">
           내일의옷장
         </div>
       </router-link>
       <router-link to="/closet/current">
-        <div id="current" class="menu" data-id="current" :class="{ 'closet_active' : activeMenuName === 'current' }">
+        <div id="current" class="menu" data-id="current" :class="{ 'closet_active' : activeMenuName === 'current' }" @click="menuClick('current')">
           현재의옷장
         </div>
       </router-link>
       <router-link to="/closet/past">
-        <div id="past" class="menu" data-id="past" :class="{ 'closet_active' : activeMenuName === 'past' }">
+        <div id="past" class="menu" data-id="past" :class="{ 'closet_active' : activeMenuName === 'past' }" @click="menuClick('past')">
           과거의옷장
         </div>
       </router-link>
       <router-link to="/closet/style">
-        <div id="style" class="menu" data-id="style" :class="{ 'closet_active' : activeMenuName === 'style' }">스타일정보</div>
+        <div id="style" class="menu" data-id="style" :class="{ 'closet_active' : activeMenuName === 'style' }" @click="menuClick('style')">스타일정보</div>
       </router-link>
       <router-link to="/closet/security">
-        <div id="mypage" class="menu" data-id="mypage" :class="{ 'closet_active' : activeMenuName === 'mypage' }">나의정보관리</div>
+        <div id="mypage" class="menu" data-id="mypage" :class="{ 'closet_active' : activeMenuName === 'mypage' }" @click="menuClick('mypage')">나의정보관리</div>
       </router-link>
       <!-- div class="menu" data-id="payment">지불정보</div -->
       <router-link to="/closet/coupon">
-        <div id="coupon" class="menu" data-id="coupon" :class="{ 'closet_active' : activeMenuName === 'coupon' }">쿠폰</div>
+        <div id="coupon" class="menu" data-id="coupon" :class="{ 'closet_active' : activeMenuName === 'coupon' }" @click="menuClick('coupon')">쿠폰</div>
       </router-link>
       <router-link to="/closet/cs">
-        <div id="cs" class="menu" data-id="cs" :class="{ 'closet_active' : activeMenuName === 'cs' }">문의내역</div>
+        <div id="cs" class="menu" data-id="cs" :class="{ 'closet_active' : activeMenuName === 'cs' }" @click="menuClick('cs')">문의내역</div>
       </router-link>
       <router-link to="/closet/notice">
-        <div id="notice" class="menu" data-id="notice" :class="{ 'closet_active' : activeMenuName === 'notice' }">공지사항</div>
+        <div id="notice" class="menu" data-id="notice" :class="{ 'closet_active' : activeMenuName === 'notice' }" @click="menuClick('notice')">공지사항</div>
       </router-link>
     </div>
   </div>
@@ -124,6 +124,7 @@ export default {
 @media screen and (max-width: 1024px) {
   .closet-mobile-menu {
     position: relative;
+    overflow-y: hidden !important;
     -webkit-overflow-scrolling: touch !important;
   }
 
