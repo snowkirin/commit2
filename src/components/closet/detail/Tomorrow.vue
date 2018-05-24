58faae9cc96a6e0a6a04ee449c75999e42a5f501
Ellen <ellen0820@onionground.com>
2018-05-24 15:23:34 +0900

    Modify css 수정

--- src/components/closet/detail/Tomorrow.vue
+++ src/components/closet/detail/Tomorrow.vue
@@ -443,13 +443,13 @@ export default {
 }
 
 .closet-card-text-title-text {
-  font-size: 18px;
-  letter-spacing: -1px;
+  font-size: 16px;
+  letter-spacing: -0.4px;
   color: #333333;
 }
 
 .closet-card-hashtag {
-  font-size: 18px;
+  font-size: 16px;
   line-height: 1.4;
   letter-spacing: -0.4px;
   text-align: left;
