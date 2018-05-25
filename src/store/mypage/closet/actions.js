0a2f90caa3afca29eab2fd63efa422c8c52ab219
Ellen <ellen0820@onionground.com>
2018-05-25 15:32:23 +0900

    Modify 현재의 옷장 데이터 없을 시 예외처리

--- src/store/mypage/closet/actions.js
+++ src/store/mypage/closet/actions.js
@@ -53,11 +53,14 @@ const setCurrentCloset = async ({ commit }) => {
   try {
     const result = await Closet.mypageCurrentCloset();
 
-    result.data.result[0].images = result.data.images;
-
-    if (result.data.result.length > 0) commit(types.SET_CURRENT_CLOSET, result.data.result[0]);
-    else if (result.data.result.length === 0) commit(types.SET_CURRENT_NONE);
-    else alert('시스템에 문제가 발생했습니다.\n잠시 후 다시 시도해주세요.');
+    if (result.data.result.length > 0) {
+      result.data.result[0].images = result.data.images;
+      commit(types.SET_CURRENT_CLOSET, result.data.result[0]);
+    } else if (result.data.result.length === 0) {
+      commit(types.SET_CURRENT_NONE);
+    } else {
+      alert('시스템에 문제가 발생했습니다.\n잠시 후 다시 시도해주세요.');
+    }
   } catch (e) {
     console.error(e.message);
   }
