747d3e50b1f28461b10863dac1987246083d0941
Jim <binary_js@onionground.com>
2018-09-04 21:00:48 +0900

    - Fix

--- src/store/signup/actions.js
+++ src/store/signup/actions.js
@@ -76,6 +76,9 @@ export default {
         console.log(res);
         commit(types.POST_JOIN);
       } else {
+        if (res.status === 500) {
+          alert('통신 오류입니다.');
+        }
         if (res.data.errorMessage) {
           const errorMessage = JSON.stringify(res.data.errorMessage.details);
           console.error(errorMessage);
