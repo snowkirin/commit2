2c662ef041bdd003e867b7f6dbfb15facf49c304
Jim <binary_js@onionground.com>
2018-10-10 18:04:13 +0900

    [x] Service Worker Change

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,5 +1,37 @@
 /* eslint-disable no-console */
 
+const updated = () => {
+  console.log('Has new content!');
+  // use window.location.reload(true) to get new cache
+};
+
+const register = async path => {
+  const registration = await navigator.serviceWorker.register(path);
+  console.log('SW registred!');
+
+  const cacheList = await caches.keys();
+  cacheList.forEach(async cacheName => {
+    if (!cacheName.includes('-temp')) return;
+
+    const tempCache = await caches.open(cacheName);
+    const tempCachesKeys = await tempCache.keys();
+    if (tempCachesKeys.length > 0) {
+      updated();
+    }
+  });
+
+  registration.onupdatefound = () => updated();
+};
+
+const { NODE_ENV, BASE_URL } = process.env;
+
+if (NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
+    register(`${BASE_URL}service-worker.js`);
+  }
+}
+
+/*
 import { register } from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
@@ -13,8 +45,9 @@ if (process.env.NODE_ENV === 'production') {
     cached() {
       console.log('Content has been cached for offline use.');
     },
-    updated() {
+    updated(reg) {
       console.log('New content is available; please refresh.');
+      reg.update();
     },
     offline() {
       console.log(
@@ -26,3 +59,4 @@ if (process.env.NODE_ENV === 'production') {
     }
   });
 }
+*/
